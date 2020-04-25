8cb4ead7750efea8287fbd4b30849f1a7cf7ea2f
Cedric <cedric.huzhiguo@gmail.com>
2020-04-24 22:03:34 -0400

    code for user info

--- com.juwanaolang.srqj/src/service.js
+++ com.juwanaolang.srqj/src/service.js
@@ -1,10 +1,16 @@
 import fetch from '@system.fetch'
 import storage from '@system.storage'
 
+
+const SERVER_END_POINT = "https://m.taozhuo.com/fast/createUser";
+const SERVER_END_POINT_CREATE_USER = `${SERVER_END_POINT}/fast/createUser`;
+const SERVER_END_POINT_LOGIN = `${SERVER_END_POINT}/fast/AjaxLogin`;
+
 class UserTokenService{
 
     constructor(){
-        this.storageKey = "userInfo"
+        this.storageKey = "userInfo";
+        this.prepareUser();
     }
 
     async hasInvitations(){
@@ -12,17 +18,116 @@ class UserTokenService{
         return invitations.length>0 ;
     }
 
-    async getUserToken (){
-        return 'token1233445565';
+    async getToken (){
+        let userInfo = await this.getUserInfo();
+        return userInfo.token || null;
+    }
+    /** 
+     * {
+     * uid: "1732260",
+     * token: "547ae77d486c2c1dd9df7a65f4a10260",
+     * sessionToken: "ob6032thape5r3sf1oe16q6pu5",
+     * "invitations":[{
+     * }]
+     * }
+     * 
+     * 
+    */
+    
+    async prepareUser(){
+        let userInfo = await this.getUserInfo();
+        if (userInfo){
+            loginResult = await this.loginUser(userInfo);
+            userInfo.sessionToken = loginResult.sessionToken;
+            userInfo.user = loginResult.user;
+        }else{
+            userInfo = await this.createNewUser();
+            userInfo.invitations = [];
+        }
+        await this.saveUserInfo(userInfo);
+        return userInfo;
     }
 
     async getUserInfo(){
-        
+        try{
+            let userInfo = await storage.get(this.storageKey);
+            console.log(`Return userInfo: ${userInfo}`);
+            return userInfo || {};
+        }catch(e){
+            console.log(`Cannot get userInfo: ${e}`);
+            return null;
+        }
+    }
+
+    async saveUserInfo(userInfo){
+        await storage.set(this.storageKey, userInfo);
+    }
+
+
+    async createNewUser(){
+        const response = await fetch.fetch({
+            url: SERVER_END_POINT_CREATE_USER,
+            method: 'post'
+        });
+        const json = await response.json();
+        console.log(`User creation response: ${json}`);
+    /**
+     * Response example:
+     * {
+    "code": 200,
+    "msg": "登录成功",
+    "data": {
+        "uid": "1732260",
+        "token": "547ae77d486c2c1dd9df7a65f4a10260",
+        "sessionToken": "ob6032thape5r3sf1oe16q6pu5"
+    }
+}
+    */
+
+        if (json.code == 200 && json.data){
+            return json.data ;
+        }
+        throw 'Create user failed';
+    }
+
+    async loginUser({uid, token}){
+        const FormData = require('form-data');
+        const form = new FormData();
+        form.append('uid', uid).append('token', token);
+
+        const response = await fetch.fetch({
+            url: SERVER_END_POINT_LOGIN,
+            method: 'POST',
+            body: form 
+        });
+        const json = await response.json();
+        console.log(`User login response: ${json}`);
+        /**
+         * {
+    "code": 200,
+    "msg": "登录成功",
+    "data": {
+        "sessionToken": "ob6032thape5r3sf1oe16q6pu5",
+        "userInfo": {
+            "id": "1732260",
+            "name": "快易_774123"
+        }
+    }
+}
+         */
+        if (json.code == 200 && json.data){
+            return json.data ;
+        }
+        throw 'Login user failed!';
+
     }
 
     async getInvitations(){
-        return [];
+        let userInfo = await this.getUserInfo() || {};
+        return userInfo.invitations || [];
     }
+
+
     async setInvitations(invitations){
 
     }
